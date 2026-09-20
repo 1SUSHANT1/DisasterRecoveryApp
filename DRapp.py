ecf8aa29e1afc078ccd2ca8f8bd5754d94740976
sushant <sushantadk33@gmail.com>
2026-09-20 03:05:22 -0400

    Services installed

--- DRapp.py
+++ DRapp.py
@@ -1,3 +1,20 @@
 import subprocess
+import getpass
 
-subprocess.run(["apt", "install","-y", "nginx"])
+def getService(service):
+	result=subprocess.run(["apt", "install","-y", service])
+	return result.returncode
+
+
+
+###################main########################
+user=getpass.getuser()
+if user != "root":
+	print("Insufficient Priviledges. Please run as root. Abort")
+	exit()
+
+
+ng=getService("nginx")
+gn=getService("gunicorn")
+fl=getService("python3-flask")
+
